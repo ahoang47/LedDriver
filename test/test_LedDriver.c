693720f26db1dc2cf242a0918718ea6dcc22ceb1
ahoang47 <austin.hoang1998@gmail.com>
2020-03-27 20:58:16 -0700

    Added new test cases

--- test/test_LedDriver.c
+++ test/test_LedDriver.c
@@ -1,9 +1,11 @@
 #include "unity.h"
 
 #include "LedDriver.h"
+static uint16_t virtualLeds = 0xFF; 
 
 void setUp(void)
 {
+	init_Led(&virtualLeds);
 }
 
 void tearDown(void)
@@ -12,7 +14,28 @@ void tearDown(void)
 
 void test_InitLed(void)
 {
-	uint16_t virtualLeds = 0xFF;
-	init_Led(&virtualLeds);//comment
 	TEST_ASSERT_EQUAL_HEX8(0, virtualLeds);
+}
+
+void test_TurnOn1(void)
+{
+	led_TurnOn(1);
+	TEST_ASSERT_EQUAL_HEX8(1, virtualLeds);	
+}
+
+void test_TurnOff1(void)
+{
+	led_TurnOff(1);
+	TEST_ASSERT_EQUAL_HEX8(0, virtualLeds);
+}
+
+void test_TurnOnMultiple(void)
+{
+	led_TurnOn(1);
+	led_TurnOn(2);
+	TEST_ASSERT_EQUAL_HEX8(0x03, virtualLeds);
+	init_Led(&virtualLeds);
+	led_TurnOn(7);
+	led_TurnOn(6);
+	TEST_ASSERT_EQUAL(0b0000000001100000, virtualLeds);
 }
