c6c779fb066c952de16b7a7ed5dd4885106d392a
Austin <austin.hoang1998@gmail.com>
2020-04-02 14:03:11 -0700

    Added runtime error for out of bounds LEDs to pass test.
    Used runtime error macro to call runtime error for display to the user during out of bounds calls. Passes all tests in test_LedDriver.c

--- src/LedDriver.c
+++ src/LedDriver.c
@@ -1,36 +1,55 @@
 #include "LedDriver.h"
 #include <stdint.h>
+#include "RuntimeErrorStub.h"
+#include <stdbool.h>
+#define FIRST_LED 1
+#define LAST_LED 16
 
 static uint16_t * LedAddress;
+static uint16_t ledImage;
+static bool isLedOOB(int ledNumber)
+{
+	if (ledNumber < FIRST_LED || ledNumber > LAST_LED)
+	{
+		RUNTIME_ERROR("LED Driver: out-of-bounds LED", ledNumber);
+		return true;
+	}
+	else return false;
+}
+
 static uint16_t ledIndexToBit(int ledNumber)
 {
 	return (1 << (ledNumber-1));
 }
-static uint16_t ledImage;
+
+static void updateHardware(void)
+{
+	*LedAddress = ledImage;
+}
 
 void init_Led(uint16_t *physicalAddress)
 {
 	LedAddress = physicalAddress;
 	ledImage = 0;
-	*LedAddress = ledImage;
+	updateHardware();
 }
 
 void led_TurnOn(int ledNumber)
 {
-	if (ledNumber <= 0 || ledNumber > 16) return;
+	if (isLedOOB(ledNumber)) return;
 	ledImage |= ledIndexToBit(ledNumber);
-	*LedAddress = ledImage;
+	updateHardware();
 }
 
 void led_TurnOff(int ledNumber)
 {
-	if (ledNumber <= 0 || ledNumber > 16) return;
+	if (isLedOOB(ledNumber)) return;
 	ledImage &= ~ledIndexToBit(ledNumber);
-	*LedAddress = ledImage;
+	updateHardware();
 }
 
 void led_TurnAllOn(void)
 {
 	ledImage = 0xffff;
-	*LedAddress = ledImage;
+	updateHardware();
 }
